f292105cdad58f08a3f6f5de3ad0c3b9a0cb6b8e
Harlan Williams <hrw@ku.edu>
2023-11-03 11:32:30 -0500

    partially implement some user_dao functions

--- website/sql_dao.hpp
+++ website/sql_dao.hpp
@@ -2,6 +2,7 @@
 #define __CALC_SQL_USER_DAO_HPP__
 
 #include <cstdio>
+#include <fstream>
 #include <iostream>
 #include <memory>
 #include <optional>
@@ -51,6 +52,13 @@ public:
 
     // TODO not implemented
     virtual std::optional<User> create_user(const std::string &name, const std::string &password) noexcept override {
+        try{
+            _db.execute_sql_statement(R"(INSERT INTO users (username, password) VALUES(%s, %s))", name.c_str(), password.c_str());
+        } catch (SQLError &e) {
+            // TODO later on: report it to the logger
+            std::cerr << e.what() << std::endl;
+        }
+
         return std::nullopt;
     };
 
@@ -61,7 +69,15 @@ public:
 
     // TODO not implemented
     virtual bool delete_user(int id) noexcept override {
-        return false;
+        try {
+            _db.execute_sql_statement(R"(DELETE FROM users WHERE id=%d)", id);
+        } catch (SQLError &e) {
+            // TODO later on: report it to the logger
+            std::cerr << e.what() << std::endl;
+            return false;
+        }
+
+        return true;
     }
 
     ~SQLUserDatabaseAccessObject() noexcept {}
