b5d15c9308b09c03286e0e61fa96a0ceda2d7ab8
Kara Deskin <106930951+kmdeskin@users.noreply.github.com>
2023-11-14 17:22:27 -0600

    Update user_script.hpp

--- website/user_script.hpp
+++ website/user_script.hpp
@@ -1,10 +1,56 @@
-#ifndef __CALC_USER_SCRIPT_HPP__
-#define __CALC_USER_SCRIPT_HPP__
+//scripting service
+#ifndef __CALC_SCRIPTING_SERVICE_HPP__
+#define __CALC_SCRIPTING_SERVICE_HPP__
 
-#include <string>
+#include <iostream>
+#include <vector>
+#include <functional>
+#include <stdexcept>
 
-struct UserScript {
-    std::string _script;
+class ScriptingService {
+private:
+    std::vector<std::function<double()>> scripts; // Store user-defined scripts
+
+public:
+    ScriptingService() {}
+
+    //new script to the service
+    void addScript(const std::function<double()>& script) {
+        scripts.push_back(script);
+    }
+
+    //execute all scripts and return results
+    std::vector<double> executeScripts() const {
+        std::vector<double> results;
+        for (const auto& script : scripts) {
+            try {
+                double result = script(); //execute the script
+                results.push_back(result);
+            } catch (const std::exception& e) {
+                //handle exceptions if the script encounters an error
+                std::cerr << "Script execution error: " << e.what() << std::endl;
+            }
+        }
+        return results;
+    }
+
+    //clear all scripts
+    void clearScripts() {
+        scripts.clear();
+    }
 };
 
-#endif // __CALC_USER_SCRIPT_HPP__
+#endif
+
+/*
+Input:
+Script: A user-defined function or operation that performs a calculation using the calculator's functionalities.
+Use Case: Allows users to automate and customize calculations by creating custom scripts.
+Functionality:
+addScript(): Adds a user-defined script (function) that can perform calculations using the calculator's functionalities.
+executeScripts(): Executes all added scripts and returns their results.
+clearScripts(): Clears all user-defined scripts.
+Example:
+Input: Functions or operations utilizing the calculator's capabilities, such as add, subtract, multiply, divide, etc., in custom user-defined functions.
+Functionality: Users create scripts that use the calculator's functions to perform complex or specific calculations automatically.
+*/
