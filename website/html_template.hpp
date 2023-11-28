bd4a46e2b7ce45b23ff4f2825758305735e237ac
Kara Deskin <106930951+kmdeskin@users.noreply.github.com>
2023-11-28 15:45:45 -0600

    update html_template.hpp to parse

--- website/html_template.hpp
+++ website/html_template.hpp
@@ -1,42 +1,42 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <unordered_map>
 #include <inja/inja.hpp>
 
 class Templating {
 private:
-    std::unordered_map<std::string, std::string> templates;
+    std::unordered_map<std::string, inja::Template> templates;
     inja::Environment env;
 
 public:
     void load(const std::string &fileName) {
-    std::ifstream fileStream(fileName); 
-    if (fileStream) {
-        std::string fileContent((std::istreambuf_iterator<char>(fileStream)), std::istreambuf_iterator<char>());
-        // store in memory
-        templates[fileName] = fileContent;
-        fileStream.close(); // close file
-    } else {
-        std::cerr << "Unable to open file: " << fileName << std::endl;
+        std::ifstream fileStream(fileName);
+        if (fileStream) {
+            std::string fileContent((std::istreambuf_iterator<char>(fileStream)), std::istreambuf_iterator<char>());
+            // parse the file content and store in memory
+            inja::Template contentTemplate = env.parse(fileContent);
+            templates[fileName] = contentTemplate;
+            fileStream.close(); // close file
+        } else {
+            std::cerr << "Unable to open file: " << fileName << std::endl;
+        }
     }
-}
 
     std::string render(const std::string &fileName, const nlohmann::json &data) {
         // check if the template exists
         if (templates.find(fileName) == templates.end()) {
-            return "template not found";
+            return "Template not found";
         }
 
-        // template content
-        std::string templateContent = templates[fileName];
+        // get the template from memory
+        inja::Template& templateRef = templates[fileName];
 
-       
         try {
-            return env.render(templateContent, data);
+            // render the template with the provided data
+            return env.render(templateRef, data);
         } catch (const inja::RenderError &e) {
-            return "render error: " + std::string(e.what());
+            return "Render error: " + std::string(e.what());
         }
     }
 };
-
-
